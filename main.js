15a341d5f463a9aa653e0808ede687bf058dfd60
Marcio Frayze <marcio.david@serpro.gov.br>
2017-06-20 19:52:58 -0300

    Adding ruby support

--- main.js
+++ main.js
@@ -14,27 +14,60 @@ function clearConsole() {
 
 function executeCode() {
 
-    clearConsole();
+    clearConsole();   
+    printExecutionHeaderOnConsole();
     let codeToEval = editor.getValue();
 
+    let selectedLanguage = getSelectedLanguage();
+    
     try {
-        let time = new Date().toLocaleTimeString('en-US', { hour12: false, 
-                                                            hour: "numeric", 
-                                                            minute: "numeric", second: "numeric"});
+        result = null;
+        switch(selectedLanguage) {
+            case "javascript":
+                result = executeJavaScriptCode(codeToEval);
+                break;
+            case "ruby":
+                result = executeRubyCode(codeToEval);
+                break;                
+            default:
+                console.log("\nERROR:\n Language not supported: " + selectedLanguage);
+        }
 
-        console.log("Starting execution (" + time + ")...");
-
-        let result = eval(codeToEval);
-
+        /* TODO: Is there a better (intead of "typeof") way to avoid printing the object 
+           when running ruby code that returns an object? */
         if (result != null) {
             console.log(result);
-        }
+        }    
 
         console.log("Execution finished successfully.");
 
     } catch (e) {
         console.log("\nERROR:\n" + e.message + "\n");
     }
+}
+
+function executeRubyCode(codeToEval) {
+    let transpiledCodeFromRubyToJS = Opal.compile(codeToEval)
+
+    let result = eval(transpiledCodeFromRubyToJS);
+
+    // TODO: For some reason it is adding a blank link in the end.  
+
+    return result;
+}
+
+function printExecutionHeaderOnConsole() {
+    let time = new Date().toLocaleTimeString('en-US', { hour12: false, 
+                                                        hour: "numeric", 
+                                                        minute: "numeric", second: "numeric"});
+
+    console.log("Starting execution (" + time + ")...");
+}
+
+function executeJavaScriptCode(codeToEval) {
+    let result = eval(codeToEval);
+
+    return result;
 }
 
 function loadSample() {
@@ -67,8 +100,23 @@ function autoPairingChanged(el) {
 }
 
 function languageSelectChanged(el) {
-    alert("Other languages support is not implemented yet. Only javascript :(");
-    el.selectedIndex = 0;
+    
+    let selectedLanguage = el.value;
+
+    switch(selectedLanguage) {
+        case "javascript":
+            editor.getSession().setMode("ace/mode/javascript");
+            break;
+        case "ruby":
+            editor.getSession().setMode("ace/mode/ruby");
+            break;
+        default:
+            console.log("\nERROR:\n Language not supported: " + selectedLanguage);           
+    }    
+}
+
+function getSelectedLanguage() {
+    return document.getElementById("selectLanguage").value;
 }
 
 window.onload = function() { 
