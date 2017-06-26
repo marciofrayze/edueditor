2ae1fd36f4b296fb999f2c4b47a59f767add871d
Marcio Frayze <marcio.david@serpro.gov.br>
2017-06-26 15:51:49 -0300

    Removing awesome font and adding basic cache

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Service worker registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
